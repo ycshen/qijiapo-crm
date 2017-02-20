ea305cc1a9c2123bc12aab3cbf5a09cc7fca1b29
shenyuchuan <shenyuchuan@itiaolingcom>
2017-02-20 15:02:03 +0800

    add

---
+++ src/main/java/com/brp/entity/CustomerEntity.java
@@ -0,0 +1,14 @@
+package com.brp.entity;
+/** 
+ * <p>Project: qijiapo-crm</p> 
+ * <p>Title: CustomerEntity.java</p> 
+ * <p>Description: TODO</p> 
+ * <p>Copyright (c) 2016 xjw Consultancy Services</p>
+ * <p>All Rights Reserved.</p>
+ * @author <a href="mailto:shenyuchuan@itiaoling.com">申鱼川</a>
+ */
+public class CustomerEntity extends BaseEntity{
+	private String customerName; //客户名称
+	
+}
+
