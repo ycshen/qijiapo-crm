af0c621e62483166b1986f22a78c6751a6b0f11d
Administrator <Administrator@qijiapo>
2017-03-22 21:38:03 +0800

    123

---
+++ src/main/java/com/brp/util/query/SalesOpportunityQuery.java
@@ -0,0 +1,38 @@
+package com.brp.util.query;
+
+import com.brp.entity.SalesOpportunityEntity;
+import com.brp.model.pageutil.Page;
+
+/** 
+ * <p>Project: qijiapo-crm</p> 
+ * <p>Title: SalesOpportunityQuery.java</p> 
+ * <p>Description: TODO</p> 
+ * <p>Copyright (c) 2016 xjw Consultancy Services</p>
+ * <p>All Rights Reserved.</p>
+ * @author <a href="mailto:shenyuchuan@itiaoling.com">申鱼川</a>
+ */
+public class SalesOpportunityQuery extends Page<SalesOpportunityEntity>{
+	private String salesOpportunityName;
+	private String customerName;
+	private Integer saleStage;//销售阶段
+	public String getSalesOpportunityName() {
+		return salesOpportunityName;
+	}
+	public void setSalesOpportunityName(String salesOpportunityName) {
+		this.salesOpportunityName = salesOpportunityName;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Integer getSaleStage() {
+		return saleStage;
+	}
+	public void setSaleStage(Integer saleStage) {
+		this.saleStage = saleStage;
+	}
+	
+}
+
