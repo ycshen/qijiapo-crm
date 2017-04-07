b5c7000824839c488510d8221c6d561241d0ec8e
ycshen <1966225457@qq.com>
2017-04-07 18:18:40 +0800

    校验权证

--- src/main/java/com/brp/interceptors/InterfaceAuthInterceptor.java
+++ src/main/java/com/brp/interceptors/InterfaceAuthInterceptor.java
@@ -11,16 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by Administrator on 2017/04/07.
  */
-public class InterfaceAuthnterceptor extends HandlerInterceptorAdapter {
+public class InterfaceAuthInterceptor extends HandlerInterceptorAdapter {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InterfaceAuthnterceptor.class);
+    private static final Logger LOG = LoggerFactory.getLogger(InterfaceAuthInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
 
         super.preHandle(request,response,o);
+      /*  Class clazz = o.getClass();
 
-        
+        JSONObject jsonObject = (JSONObject) o;
+        Boolean auth = AuthUtils.auth(jsonObject);
+        if(!auth){
+            return false;
+        }*/
         return true;
 
     }
